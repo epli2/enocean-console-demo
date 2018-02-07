2b3c368a75338b600aab58dd3464ef7025fde2e6
epli2 <motoryo1@gmail.com>
2018-02-07 13:16:34 +0900

    選択中のボタンの色が変わるように

--- src/components/Temperature.vue
+++ src/components/Temperature.vue
@@ -18,11 +18,11 @@
       <chart :chartData="chartData" :height="285"></chart>
     </div>
     <div class="buttons">
-      <div class="btn-group" role="group" aria-label="Basic example">
-        <button v-on:click="setRange('1min')" type="button" class="btn btn-secondary">1分</button>
-        <button v-on:click="setRange('10min')" type="button" class="btn btn-secondary">10分</button>
-        <button v-on:click="setRange('hour')" type="button" class="btn btn-secondary">1時間</button>
-        <button v-on:click="setRange('all')" type="button" class="btn btn-secondary">全部</button>
+      <div class="btn-group" role="group">
+        <button v-on:click="setRange('1min')" type="button" class="btn btn-secondary" v-bind:class="{active: range === '1min'}">1分</button>
+        <button v-on:click="setRange('10min')" type="button" class="btn btn-secondary" v-bind:class="{active: range === '10min'}">10分</button>
+        <button v-on:click="setRange('hour')" type="button" class="btn btn-secondary" v-bind:class="{active: range === 'hour'}">1時間</button>
+        <button v-on:click="setRange('all')" type="button" class="btn btn-secondary" v-bind:class="{active: range === 'all'}">全部</button>
       </div>
     </div>
   </div>
